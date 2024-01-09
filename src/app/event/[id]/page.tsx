775d549315375bf3aab3fd8824c1d213682f935f
Rafaeldev1982 <eurafaelpereira@gmail.com>
2024-01-09 20:57:25 -0300

    Criando página do evento

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,9 +1,17 @@
 type Props = { params: { id: string; } }
 const Page = ({ params }: Props) => {
     return (
-        <div>
-            <h3>ID DO EVENTO: {params.id}</h3>
-        </div>
+        <main className="text-center mx-auto max-w-lg p-5">
+            <header>
+                <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
+                <h1 className="text=3xl mt-5 mb-2">** TITULO DO EVENTO **</h1>
+                <p className="text-sm mb-5">** DESCRIÇÃO DP EVENTO **</p>
+            </header>
+
+            ....
+
+            <footer className="mt-5 text-sm">Criado por Rafael</footer>
+        </main>
     );
 }
 export default Page;
