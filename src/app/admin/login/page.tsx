f176c0fc3fbbecacdae9519d11baa699ed80477b
Rafaeldev1982 <eurafaelpereira@gmail.com>
2024-01-17 19:51:04 -0300

    Criando o Layout-Geral

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            Painel ADM - Login
+        </div>
+    );
+}
+
+export default Page;
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            Painel ADM
+        </div>
+    );
+}
+
+export default Page;
