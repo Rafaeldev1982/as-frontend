07e9e0f78318b47956653c88e04ee849a3d58bcc
Rafaeldev1982 <eurafaelpereira@gmail.com>
2024-01-17 20:29:02 -0300

    Verificando se está logado

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,10 @@
-const Page = () => {
+import * as api from '@/api/server'
+import { redirect } from 'next/navigation';
+
+const Page = async () => {
+    const logged = await api.pingAdmin();
+    if (!logged) redirect('/admin/login');
+
     return (
         <div>
             Painel ADM
