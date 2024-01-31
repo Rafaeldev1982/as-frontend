7610b1887d852822d30e779321f9be496a7f0f44
Rafaeldev1982 <eurafaelpereira@gmail.com>
2024-01-31 19:52:44 -0300

    Criando o InputField

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
+import { InputField } from "@/components/admin/InputField";
+import { useState } from "react";
+
+
 const Page = () => {
+    const [passwordInput, setPasswordInput] = useState('');
+
     return (
         <div>
             Painel ADM - Login
+            <InputField
+                type="password"
+                value={passwordInput}
+                onChange={e => setPasswordInput(e.target.value)}
+                placeholder="Digite a senha"
+            />
         </div>
     );
 }
